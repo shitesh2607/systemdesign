32f10eeeea0b12eab9cf01a2620a80e2a25226fd
shitesh2607 <shitesh2607@gmail.com>
2025-10-21 20:10:53 +0530

    System Design Day 1

---
+++ systemdesign/src/test/java/com/systemdesign/systemdesign/SystemdesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemdesign.systemdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemdesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
